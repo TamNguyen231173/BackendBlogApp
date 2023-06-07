1266a1fb1423464fc14e3e262f092df1011e2b68
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2023-06-07 12:25:41 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,72 +13,72 @@ const pug = require("pug");
 
 const app = express();
 
-// Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, "./public/");
-const viewsPath = path.join(__dirname, "./templates/views");
-const partialsPath = path.join(__dirname, "./templates/partials");
-const helpers = path.join(__dirname, "./utils/helpers");
+// // Define paths for Express config
+// const publicDirectoryPath = path.join(__dirname, "./public/");
+// const viewsPath = path.join(__dirname, "./templates/views");
+// const partialsPath = path.join(__dirname, "./templates/partials");
+// const helpers = path.join(__dirname, "./utils/helpers");
 
-// Setup static directory to serve
-app.use(express.static(publicDirectoryPath));
+// // Setup static directory to serve
+// app.use(express.static(publicDirectoryPath));
 
-// Setup handlebars engine and views location
-app.set("view engine", "pug");
-app.set("views", viewsPath);
+// // Setup handlebars engine and views location
+// app.set("view engine", "pug");
+// app.set("views", viewsPath);
 
-// Middleware
+// // Middleware
 
-// 1. Body Parser
-app.use(express.json({ limit: "10kb" }));
+// // 1. Body Parser
+// app.use(express.json({ limit: "10kb" }));
 
-// 2. Cookie Parser
-app.use(cookieParser());
+// // 2. Cookie Parser
+// app.use(cookieParser());
 
-// 3. Logger
-if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+// // 3. Logger
+// if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
-// 4. Cors
-app.use(
-  cors({
-    origin: "http://localhost:8000",
-    credentials: true,
-  })
-);
+// // 4. Cors
+// app.use(
+//   cors({
+//     origin: "http://localhost:8000",
+//     credentials: true,
+//   })
+// );
 
-// 5. Routes
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
-app.use("", authRouter);
-app.use("/api/posts", postRouter);
-app.use("/common", commonRouter);
-app.use("/api/categories", categoryRouter);
+// // 5. Routes
+// app.use("/api/users", userRouter);
+// app.use("/api/auth", authRouter);
+// app.use("", authRouter);
+// app.use("/api/posts", postRouter);
+// app.use("/common", commonRouter);
+// app.use("/api/categories", categoryRouter);
 
 // Testing
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send(200).json({
+  res.status(200).json({
     status: "success",
     message: "Welcome to CodevoWeb😂😂👈👈",
   });
 });
 
-// UnKnown Routes
-app.all("*", (req: Request, res: Response, next: NextFunction) => {
-  const err = new Error(`Route ${req.originalUrl} not found`) as any;
-  err.statusCode = 404;
-  res.render("404Page");
-  next(err);
-});
+// // UnKnown Routes
+// app.all("*", (req: Request, res: Response, next: NextFunction) => {
+//   const err = new Error(`Route ${req.originalUrl} not found`) as any;
+//   err.statusCode = 404;
+//   res.render("404Page");
+//   next(err);
+// });
 
-// Global Error Handler
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  err.status = err.status || "error";
-  err.statusCode = err.statusCode || 500;
+// // Global Error Handler
+// app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+//   err.status = err.status || "error";
+//   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message,
+//   });
+// });
 
 app.listen(8000, () => {
   console.log(`Server started on port: 8000`);
