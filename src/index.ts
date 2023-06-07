e02485872ec20b68e5d475dec8db5c926882ebcc
TamNguyen <43509906+TamNguyen231173@users.noreply.github.com>
2023-06-07 14:13:25 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,22 +28,22 @@ app.set("views", viewsPath);
 
 // Middleware
 
-// // 1. Body Parser
-// app.use(express.json({ limit: "10kb" }));
+// 1. Body Parser
+app.use(express.json({ limit: "10kb" }));
 
-// // 2. Cookie Parser
-// app.use(cookieParser());
+// 2. Cookie Parser
+app.use(cookieParser());
 
-// // 3. Logger
-// if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+// 3. Logger
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
-// // 4. Cors
-// app.use(
-//   cors({
-//     origin: "https://backend-news-app-git-main-tamnguyen231173.vercel.app/",
-//     credentials: true,
-//   })
-// );
+// 4. Cors
+app.use(
+  cors({
+    origin: "https://backend-news-app-git-main-tamnguyen231173.vercel.app/",
+    credentials: true,
+  })
+);
 
 // // 5. Routes
 // app.use("/api/users", userRouter);
